0e52f8ea9d0082871dbcff1394af66e48a20009f
Oleg Koryagin <olegk.ua@gmail.com>
2019-12-20 23:12:54 +0200

    data source fix

--- terraform/aws-1/bac/90-data_source.tf
+++ terraform/aws-1/bac/90-data_source.tf
@@ -8,18 +8,18 @@
 #}
 
 # Ubuntu Server 18.04 LTS (HVM),EBS General Purpose (SSD) Volume Type. Support available from Canonical
-#data "aws_ami" "ubuntu-18_04" {
-#  most_recent = true
-#
-#  filter {
-#    name   = "name"
-#    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-#  }
-#
-#  filter {
-#    name   = "virtualization-type"
-#    values = ["hvm"]
-#  }
-#
-#  owners = ["099720109477"] # Canonical
-#}
+data "aws_ami" "ubuntu-18_04" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
